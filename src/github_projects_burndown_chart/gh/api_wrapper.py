6f132b5114479dfae2893ba409cabd30a4d0b2c7
Vivien Guo <guov@student.unimelb.edu.au>
2024-07-26 22:46:38 +1000

    cleanups & readme

--- src/github_projects_burndown_chart/gh/api_wrapper.py
+++ src/github_projects_burndown_chart/gh/api_wrapper.py
@@ -11,8 +11,6 @@ from config import config, secrets
 from .project import Project, ProjectV1, ProjectV2
 from .queries import OrganizationProject, OrganizationProjectV2, RepositoryProject, RepositoryProjectV2
 
-from pprint import pprint
-
 # Set up logging
 __logger = logging.getLogger(__name__)
 __ch = logging.StreamHandler()
@@ -52,10 +50,8 @@ def get_project_v2(project_type) -> Project:
 
     query_sprint = config['settings'].get('sprint')
     if query_sprint is not None:
-        print("----FILTER BY SPRINT----")
         all_items = project_data['items']['nodes']
         project_data['items']['nodes'] = filter(lambda i: i.get('sprint') and i.get('sprint')['title'] == query_sprint, all_items)
-        pprint(project_data)
 
     page_info = project_data['items']['pageInfo']
     while page_info['hasNextPage']:
